1654db60f205a14edaaf7586c8551a1470aafd6d
RaMandeep2002 <ramandeepsingh1511@gmail.com>
2022-05-27 11:53:05 +0530

    here i am working on about section and set navbar

--- js/index.js
+++ js/index.js
@@ -1,29 +1,38 @@
-var loader;
+var loader
 // here is the code for the apply button
 
 function applyNow() {
-    alert('You want a admission in this college');
-    window.location.href = 'https://admissions.cgc.ac.in/';
+  alert('You want a admission in this college')
+  window.location.href = 'https://admissions.cgc.ac.in/'
 }
 
 //here is the code for the loader
 function loadNow(opacity) {
-    if (opacity <= 0) {
-        displayContent();
-    } else {
-        loader.style.opacity = opacity;
-        window.setTimeout(function() {
-            loadNow(opacity - 0.05);
-        }, 50);
-    }
+  if (opacity <= 0) {
+    displayContent()
+  } else {
+    loader.style.opacity = opacity
+    window.setTimeout(function () {
+      loadNow(opacity - 0.05)
+    }, 50)
+  }
 }
 
 function displayContent() {
-    loader.style.display = 'none';
-    document.getElementById('content').style.display = 'block';
+  loader.style.display = 'none'
+  document.getElementById('content').style.display = 'block'
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    loader = document.getElementById('loader');
-    loadNow(1);
-});
+document.addEventListener('DOMContentLoaded', function () {
+  loader = document.getElementById('loader')
+  loadNow(1)
+})
+
+// function collapseMenu() {
+//     var x = document.getElementById('myLinks');
+//     if (x.style.display === 'block') {
+//         x.style.display = 'none';
+//     } else {
+//         x.style.display = 'block';
+//     }
+// }
